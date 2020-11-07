ca39fe244f65b194e455ff22756079a1ef3ab38d
MaxwellTav <72995619+MaxwellTav@users.noreply.github.com>
2020-11-07 04:56:23 -0400

    First Commit
    Subiendo el script.

---
+++ RandomPhrase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RandomPhrase : MonoBehaviour
+{
+    [Header("Referencias")]
+    public Text referenceText;
+    
+    [Header("Frases")]
+    public string[] phrases;
+
+    //valor que se tomara al azar
+    [HideInInspector]
+    public int i;
+    
+    void Start()
+    {
+        referenceText = GetComponent<Text>();
+        MostrarFrase();
+
+        Debug.Log("Se mostro la frase: " + phrases[i]);
+    }
+
+    public void MostrarFrase()
+    {
+        i = Random.Range(0, phrases.Length);
+
+        referenceText.GetComponent<Text>().text = phrases[i];
+    }
+}
